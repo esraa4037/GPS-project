74ab1f48d76790cafc9e41f854a14fbdf8642507
esraa4037 <esraamohamed4037@gmail.com>
2021-06-03 23:59:22 +0200

    adding while loop to main

--- distance.c
+++ distance.c
@@ -6,14 +6,43 @@ double degToRad(double deg) {
   return deg * PI / 180;
 }
 
-double distance(double latitude1, double latitude2, double longitude1,double longitude2) {
+double semiSwapLatitude (){
+    /// this function replace the second point with the first
+    latiude1 = latitude2;
+    return latitude1;
 
-  double a =  pow(sin(degToRad(latitude2-latitude1)/2),2)+ pow(sin(degToRad(longitude2-longitude1)/2),2) * cos(degToRad(latitude2) )* cos(degToRad(latitude1)); 
-  double c = 2 * atan2(sqrt(a),sqrt(1-a)); 
+}
+double semiSwapLatitude (){
+    /// this function replace the second point with the first
+    latiude1 = latitude2;
+    return latitude1;
+
+}
+double gitLatitude(){
+
+}
+
+double gitLongitude(){
+
+}
+
+////////// this function calulate the distance between two GPS points /////////////
+double distance(double latitude1, double longitude1, double latitude2 ,double longitude2) {
+
+  double a =  pow(sin(degToRad(latitude2-latitude1)/2),2)+
+	  pow(sin(degToRad(longitude2-longitude1)/2),2) * cos(degToRad(latitude2) )* cos(degToRad(latitude1)); 
+  double c = 2 * atan2(sqrt(a),sqrt(1-a));
   return 6371 * c;
 }
 int main () {
-//    double x, ret, val;
+
+////////////////// calculate the total distance ////////////
+int total_distance = 0;
+
+		while (GPIO_PORTF_DATA_R & 0x00000001 != 0)
+        {
+            total_distance += distance(gitLatitude, gitLongitude,  );
+        }
 
 //    x = 45.0;
 //    val = PI / 180;
