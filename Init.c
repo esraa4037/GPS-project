ab21bec2d248585ecf40f38645fdbd96e138f368
Fatma-Badr <1806734@eng.asu.edu.eg>
2021-06-04 15:01:35 +0200

    Ports_Init continuous

--- Init.c
+++ Init.c
@@ -20,7 +20,7 @@ void PortE_Init(){
 SYSCTL_RCGCGPIO_R |=0x10;
 while((SYSCTL_PRGPIO_R & 0x10)== 0);
 GPIO_PORTE_CR_R |= PE0123_mask;
-GPIO_PORTE_DIR_R &=~ PE0123_mask;
+GPIO_PORTE_DIR_R |= PE0123_mask;
 GPIO_PORTE_DEN_R |= PE0123_mask;
 GPIO_PORTE_AMSEL_R &=~ PE0123_mask;
 GPIO_PORTE_AFSEL_R &=~ PE0123_mask;
@@ -28,3 +28,41 @@ GPIO_PORTE_PCTL_R &=~ 0xffff;
 GPIO_PORTE_DATA_R &=~ PE0123_mask;	
 }
 
+void Sw2_Init(){
+SYSCTL_RCGCGPIO_R |=0x20;
+while ((SYSCTL_PRGPIO_R &0x20) == 0);
+GPIO_PORTF_LOCK_R =	Lock_key;
+GPIO_PORTF_CR_R |= PF0_mask;
+GPIO_PORTF_DIR_R &=~ PF0_mask;
+GPIO_PORTF_DEN_R |= PF0_mask;
+GPIO_PORTF_AMSEL_R &=~ PF0_mask;
+GPIO_PORTF_AFSEL_R &=~ PF0_mask;
+GPIO_PORTF_PCTL_R &=~ 0xF;	
+GPIO_PORTF_PUR_R |=PF0_mask;	
+}
+
+void Rled_Init(){
+SYSCTL_RCGCGPIO_R |=0x20;
+while ((SYSCTL_PRGPIO_R &0x20) == 0);
+GPIO_PORTF_LOCK_R =	Lock_key;
+GPIO_PORTF_CR_R |= PF1_mask;
+GPIO_PORTF_DIR_R |= PF1_mask;
+GPIO_PORTF_DEN_R |= PF1_mask;
+GPIO_PORTF_AMSEL_R &=~ PF1_mask;
+GPIO_PORTF_AFSEL_R &=~ PF1_mask;
+GPIO_PORTF_PCTL_R &=~ 0xf0;	
+GPIO_PORTF_DATA_R &=~ PF1_mask;	
+	
+}
+
+void SystemInit(){}
+
+
+int main(){
+Rled_Init();	
+Sw2_Init();
+PortD_Init();
+PortE_Init();	
+
+	
+}
