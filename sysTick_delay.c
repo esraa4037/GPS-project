d92e16c860c5c1527f5c3e8af58364181b97d65b
esraa4037 <esraamohamed4037@gmail.com>
2021-06-08 09:04:51 +0200

    just added the system stick delay

---
+++ sysTick_delay.c
@@ -0,0 +1,72 @@
+// control and status register
+// reload register
+// current value register
+
+
+// Enable = 0;
+// reload value = the number of counts;    Sys_Tick_LOAD   // 24-bit
+
+// Sys_Tick_VAL = 0;    // that clears the current register and clears the count flag  // READ ONLY  // 24-bit
+           //we can also type any value
+// Enable = 1;
+// clock source = 1;
+
+
+
+void SystemInit() {}
+
+void portF_init(void) {
+    uint32_t delay;
+    SYSCTL_RCGCGPIO_R |= 0x20;
+    delay = 1;
+    GPIO_PORTF_LOCK_R = 0X4C4F434B;
+    GPIO_PORTF_RC_R = 0X1F;
+    GPIO_PORTF_AFSEL_R = 0;
+    GPIO_PORTF_PCTL_R = 0;
+    GPIO_PORTF_AMSEL_R = 0;
+    GPIO_PORTF_DEN_R = 0X1F;
+    GPIO_PORTF_DIR_R = 0X0E;
+    GPIO_PORTF_PUR_R = 0X11;
+
+}
+
+// initialization for systick timer
+void sysTick_init()
+{
+    NVIC_ST_CTRL_R = 0;  //DISABLE TIMER
+    NVIC_ST_RELOAD_R = 16000000 - 1;
+    NVIC_ST_CURRENT_R = 0;  // clears the current register and the count flag
+    NVIC_ST_CTRL_R = 5;  // set the enable and clock source to 1
+
+}
+
+// this function makes a delay in seconds
+//it takes the number of seconds and number of counts that makes a one second delay(about 16,000,000 counts)
+void delay_in_seconds(int seconds, uint32_t counts)
+{
+    int i;
+    for (i=0; i<seconds; i++){
+        sysTick_delay(counts);
+    }
+}
+
+void sysTick_delay(uint32_t counts){
+    NVIC_ST_CTRL_R = 0;  //DISABLE TIMER
+    NVIC_ST_RELOAD_R = counts;
+    NVIC_ST_CURRENT_R = 0;  // clears the current register and the count flag
+    NVIC_ST_CTRL_R = 5;  // set the enable and clock source to 1
+
+    if (NVIC_ST_CTRL_R & 0x10000 == 0) { }
+
+}
+
+int main()
+{
+    portF_init();
+    sysTick_init();
+    while(1){
+        GPIO_PORTF_DATA_R |= 0x02;
+        delay_in_seconds(5, 16000000);
+    }
+}
+
