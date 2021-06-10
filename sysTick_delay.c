b78edce0cd014cdd4cbdcdba4c2af02b88f83240
esraa4037 <71180605+esraa4037@users.noreply.github.com>
2021-06-10 11:13:54 +0200

    Update sysTick_delay.c

--- sysTick_delay.c
+++ sysTick_delay.c
@@ -5,7 +5,7 @@ void portF_init(void) {
     SYSCTL_RCGCGPIO_R |= 0x20;
     delay = 1;
     GPIO_PORTF_LOCK_R = 0X4C4F434B;
-    GPIO_PORTF_RC_R = 0X1F;
+   // GPIO_PORTF_RC_R = 0X1F;
     GPIO_PORTF_AFSEL_R = 0;
     GPIO_PORTF_PCTL_R = 0;
     GPIO_PORTF_AMSEL_R = 0;
@@ -16,16 +16,16 @@ void portF_init(void) {
 }
 
 // initialization for systick timer
-/*
-void sysTick_init()
+
+void one_second_delay()
 {
     NVIC_ST_CTRL_R = 0;  //DISABLE TIMER
     NVIC_ST_RELOAD_R = 16000000 - 1;
     NVIC_ST_CURRENT_R = 0;  // clears the current register and the count flag
     NVIC_ST_CTRL_R = 5;  // set the enable and clock source to 1
-
+    if (NVIC_ST_CTRL_R & 0x10000 == 0) { }
 }
-*/
+
 
 
 // this function makes a delay in seconds
@@ -34,10 +34,10 @@ void delay_in_seconds(int seconds, uint32_t counts)
 {
     int i;
     for (i=0; i<seconds; i++){
-        sysTick_delay(counts);
+        one_second_delay();
     }
 }
-
+/*
 void sysTick_delay(uint32_t counts){
     NVIC_ST_CTRL_R = 0;  //DISABLE TIMER
     NVIC_ST_RELOAD_R = counts;
@@ -47,14 +47,15 @@ void sysTick_delay(uint32_t counts){
     if (NVIC_ST_CTRL_R & 0x10000 == 0) { }
 
 }
+*/
 
 int main()
 {
     portF_init();
-    sysTick_init();
+    
     while(1){
         GPIO_PORTF_DATA_R ^= 0x02;
-        delay_in_seconds(5, 16000000);
+        delay_in_seconds(5);
     }
 }
 
