7659d90f2e9fa1002d7272f6e97d6f85a10ddaa4
bassant1999 <72526468+bassant1999@users.noreply.github.com>
2021-06-12 23:38:04 +0200

    Add files via upload

--- read.c
+++ read.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "stdint.h"
 #include <tm4c123gh6pm.h>
-#define MAX 500
+#define MAX 200
 int avalible(){
 	return ((UART2_FR_R  & (1<<4)) ==0)?0:1;
 }
@@ -14,29 +14,24 @@ char* readStr(){
 	UART0_CTL_R|=0x301;
 	char str[MAX];
 	char cstr;
-	while(readChar()!="$");
-	for(int i=0;i<MAX;i++){
-		cstr=readChar();
-		if(cstr=="\n" || cstr=="\r")
-		    break;
-		else
-			str[i]=cstr;
-	UART2_CTL_R|=0x201;//disable rx here
-     return str;
+	while(readChar()!='$');
+	if(readChar()=='G'){
+		if(readChar()=='p'){
+			if(readChar()=='G'){
+				if(readChar()=='G'){
+					if(if(readChar()=='A')){
+						    str[0:5]="$GPGGA";//NOT SURE
+							for(int i=0;i<MAX;i++){
+		                        cstr=readChar();
+		                        if(cstr=="\n" || cstr=="\r")
+		                            break;
+		                        else
+			                    str[i]=cstr;
+					             }
+				}
+			}
+		}
 	}
 
 }
-/*
-UART0_CTL_R|=0x301;
-while((UART2_FR_R  & (1<<4))!=0){
-	if(UART2_DR_R !="$")
-	    continue;
-	else if(UART2_DR_R =="\n" || UART2_DR_R !="\r")
-	    break;
-	else if{
-		str[i]=UART2_DR_R;
-	}
-	UART2_CTL_R|=0x201;//disable rx here
-
 }
-*/
