5ab13a43b52d47ca90d400bd7a6276e2fc23c640
Fatma-Badr <85190955+Fatma-Badr@users.noreply.github.com>
2021-06-13 02:29:19 +0200

    Update read.c
    update read.c

--- read.c
+++ read.c
@@ -3,35 +3,54 @@
 #include "stdint.h"
 #include <tm4c123gh6pm.h>
 #define MAX 200
-int avalible(){
-	return ((UART2_FR_R  & (1<<4)) ==0)?0:1;
+
+int availble(){
+	return ((UART2_FR_R  & (1<<4)) ==0)?1:0;
 }
 char readChar(){
 	while(availble()!=1);
 	return UART2_DR_R & 0xFF;
 }
 char* readStr(){
-	UART0_CTL_R|=0x301;
-	char str[MAX];
+	UART2_CTL_R|=0x301;
+	//char str[MAX];
 	char cstr;
-	while(readChar()!='$');
+	int valid=1;
+  while(valid==1){
+	while(readChar()!='$'){};
+  str[0]='$';
 	if(readChar()=='G'){
+    str[1]='G';
 		if(readChar()=='p'){
+      str[2]='p';
 			if(readChar()=='G'){
+        str[3]='G';
 				if(readChar()=='G'){
-					if(if(readChar()=='A')){
-						    str[0:5]="$GPGGA";//NOT SURE
-							for(int i=0;i<MAX;i++){
-		                        cstr=readChar();
-		                        if(cstr=="\n" || cstr=="\r")
-		                            break;
-		                        else
+          str[4]='G';
+					if(readChar()=='A'){
+                        str[5]='A';
+                        valid=0;
+                        for(int i=6;i<MAX;i++){
+		                    cstr=readChar();
+		                    if(cstr=='\n' || cstr=='\r')
+		                        break;
+		                    else
 			                    str[i]=cstr;
+
 					             }
 				}
 			}
 		}
 	}
+     
 
 }
 }
+UART2_CTL_R|=0x001;
+return str;
+
+
+
+
+
+  
