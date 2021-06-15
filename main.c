6ff2079febf107e2b2cefe57a8d815f89ee98253
bassant1999 <72526468+bassant1999@users.noreply.github.com>
2021-06-15 14:44:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,26 +7,23 @@ PortE_Init();
 UART_Init();
 	
 
-//total_distance();
+//track the distance
  delay_in_seconds(30);//delay for GPS fix
 double total_distance = 0;
-char* first = readStr();    //To read the start point of the jurney {latitude1 , longitude1}
-st=GPS_time(first);
-parseLat (first);
-parseLng (first);
-double lat1=GPS_decimal( lat , latDir(first));
-double lon1=GPS_decimal( lng  , lngDir(first));
-    // int startPoin[1] = parse(start);  //int startPoin[1] = {lat1, lon1};
-while ( (GPIO_PORTF_DATA_R & 0x01) != 0)
+char* first = readStr();    //read first point from GPS
+st=GPS_time(first);         
+parseLat (first);          //get latitude from GPS format for the first point
+parseLng (first);          //get longitude from GPS format for the first point
+double lat1=GPS_decimal( lat , latDir(first));  //get real value of latitude from GPS format for the first point
+double lon1=GPS_decimal( lng  , lngDir(first)); //get real value of longitude from GPS format for the first point
+while ( (GPIO_PORTF_DATA_R & 0x01) != 0)        // looping until pushing SW2
         {
-	char* second = readStr();
-	en=GPS_time(second);
-	parseLat (second);
-	 parseLng (second);
-	 double lat2=GPS_decimal( lat , latDir(second));
-	 double lon2=GPS_decimal( lng  , lngDir(second));
-            // string end = read();
-            // int second_point[1] = parse(end);  //int second_point[1] = {lat2, lon2};
+	char* second = readStr();               //read second point from GPS
+	en=GPS_time(second);                    
+	parseLat (second);                       //get latitude from GPS format for the second point
+	 parseLng (second);                      //get longitude from GPS format for the second point
+	 double lat2=GPS_decimal( lat , latDir(second)); //get real value of latitude from GPS format for the second point
+	 double lon2=GPS_decimal( lng  , lngDir(second)); //get real value of longitude from GPS format for the fsecond point
         total_distance += distance(lat1, lon1,lat2,lon2); // increment the total distance
 	 print((int)total_distance);//to display the distance in 7 segment during the movement
 	led(total_distance);// if total_distance>=100, the red led turns on
